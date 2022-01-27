a9a9f7b2feec8bf0ac22e440a1d88863b92b878a
mboser <mboser@iastate.edu>
2022-01-26 22:45:54 -0600

    Got Knights Tour working and cleaned up unused files

--- Homework0/knights_tour.c
+++ Homework0/knights_tour.c
@@ -1,42 +1,66 @@
 #include <stdio.h>
-#include <string.h>
-
-char* jump(int x, int y, int visited[],int numJumps, int curr)
+ 
+int valid_move(int x, int y, int grid[5][5])
 {
-    if (numJumps == 25){
-        printf("%d, ", visited[numJumps]);
-        jump(visited, numJumps-1, curr);
-    }
-
-    reutrn NULL;
+    return (x >= 0 && x < 5 && y >= 0 && y < 5 && grid[x][y] == -1);
+}
+ 
+void reset_board(int grid[5][5]){
+    for (int x = 0; x < 5; x++)
+        for (int y = 0; y < 5; y++)
+            grid[x][y] = -1;
 }
 
-int is_valid(int x, int y)
+void tour(int x, int y, int num_jumps, int grid[5][5], int x_moveset[5], int y_moveset[5], int path[25])
 {
-    if (x > 0 && x <= 5 && y > 0 && y <= 5) {
-        return 0;
+
+    grid[x][y] = num_jumps;
+    int curr = (x+1)+(y*5);
+    path[num_jumps-1] = curr;
+    int k, next_x, next_y;
+    if (num_jumps == 25){
+        int i;
+        for(i=0;i<24;i++){
+            printf("%d, ", path[i]);
+        }
+        printf("%d", path[24]); //print last one without the comma
+        printf("\n");
+        grid[x][y] = -1;
+        return;
     }
-    else {
-        return 1;
+
+
+    for (k = 0; k < 8; k++) { //do all the moves in the set of possible moves
+        next_x = x + x_moveset[k];
+        next_y = y + y_moveset[k];
+        if (valid_move(next_x, next_y, grid)) {
+            tour(next_x, next_y, num_jumps + 1, grid, x_moveset, y_moveset, path);
+        }
     }
+    grid[x][y] = -1;
+
 }
 
-int main(int argc, char *argv[])
+ 
+int main()
 {
     int grid[5][5];
-    int i, j;
-    int count = 1;
-    int visited_arr[25];
-    for (j = 0; j < 5; j++){
-        for (i = 0; i < 5; i++){
-            grid[j][i]=count;
-            jump(visited_arr, 0, count);
-            //printf("%3d", grid[j][i]);
-
-            count++;
-        }
-        printf("\n");
-    }
+ 
     
-    return 0; 
+    int x_moveset[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+    int y_moveset[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+    int i,j;
+
+    for(i = 0; i < 5; i++){
+        for(j = 0; j< 5; j++){
+            reset_board(grid);
+            int path[25];     
+            
+            grid[i][j] = 0;   
+            tour(i, j, 1, grid, x_moveset, y_moveset, path);
+        }
+    }
+
+    return 0;
 }
